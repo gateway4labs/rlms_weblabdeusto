3256a50fde4d0a3d5e5a31575a1f189714bd359a
Pablo Orduña <pablo.orduna@deusto.es>
2015-05-31 03:13:51 +0200

    Use laboratory.laboratory_id

--- g4l_rlms_weblabdeusto/__init__.py
+++ g4l_rlms_weblabdeusto/__init__.py
@@ -267,8 +267,8 @@ class RLMS(BaseRLMS):
 
 
 def populate_cache(rlms):
-    for lab_id in rlms.get_laboratories():
-        rlms.get_translations(lab_id)
+    for laboratory in rlms.get_laboratories():
+        rlms.get_translations(laboratory.laboratory_id)
 
 WEBLAB_DEUSTO = register("WebLab-Deusto", ['5.0'], __name__)
 WEBLAB_DEUSTO.add_local_periodic_task('Populating cache', populate_cache, minutes = 55)
